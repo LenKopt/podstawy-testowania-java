3faae2420ebf0e927eb5f19634f0bda80db36f0b
Alena Iadkouskaya <koptewka@mail.ru>
2024-02-18 14:19:09 +0100

    implementations for all tests

--- src/main/java/pl/akademiaspecjalistowit/podstawytestowaniajava/ex2/money/Money.java
+++ src/main/java/pl/akademiaspecjalistowit/podstawytestowaniajava/ex2/money/Money.java
@@ -6,7 +6,7 @@ public class Money {
     public final Double amount;
 
     public Money(Currency currency, Double amount) {
-        if(amount < 0){
+        if (amount < 0) {
             throw new MoneyException("Money cannot hold negative amount");
         }
         this.currency = currency;
--- src/test/java/pl/akademiaspecjalistowit/podstawytestowaniajava/ex2/BankServiceTest.java
+++ src/test/java/pl/akademiaspecjalistowit/podstawytestowaniajava/ex2/BankServiceTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.akademiaspecjalistowit.podstawytestowaniajava.ex2.money.Currency;
 import pl.akademiaspecjalistowit.podstawytestowaniajava.ex2.money.Money;
+import pl.akademiaspecjalistowit.podstawytestowaniajava.ex2.money.MoneyException;
+
+import java.util.List;
 
 class BankServiceTest {
 
@@ -32,31 +35,91 @@ class BankServiceTest {
 
     @Test
     public void should_withdraw_money_successfully() {
+        //given
+        bankServiceSuT.depositMoney(new Money(Currency.PLN, 100.0));
+        Money money = new Money(Currency.PLN, 10.0);
+        Double resultOperation = getSummaMoney(bankServiceSuT.checkBalance()) - money.amount;
 
+        //when
+        bankServiceSuT.withdrawMoney(money);
+
+        //then
+        assertThat(getSummaMoney(bankServiceSuT.checkBalance())).isEqualTo(resultOperation);
+    }
+
+    private Double getSummaMoney(List<Money> listMoney) {
+        Double sum = 0.0;
+        for (Money nextItem : listMoney) {
+            sum += nextItem.amount;
+        }
+        return sum;
     }
 
     @Test
     public void should_not_allow_to_withdraw_money_on_debit() {
-
+        //rozumiem ten test tak, że nie możno pobrać więcej niż saldo
+        //given
+        bankServiceSuT.depositMoney(new Money(Currency.PLN, 100.0));
+        Money money = new Money(Currency.PLN, 110.0);
+        Double resultOperation = getSummaMoney(bankServiceSuT.checkBalance()) - money.amount;
+        //when
+        Money moneyEnd = bankServiceSuT.withdrawMoney(money);
+        //then
+        try {
+            //nie wiem co moge tutaj napisac
+        } catch (MoneyException thrown) {
+            assertThat(thrown.getMessage()).isNotEqualTo("");
+        }
     }
 
     @Test
     public void balance_should_not_change_when_withdraw_failed() {
-
+        //given
+        Double currentBalance = getSummaMoney(bankServiceSuT.checkBalance());
+        Money money = new Money(Currency.PLN, currentBalance * 10 / 100);
+        //when
+        bankServiceSuT.withdrawMoney(money);
+        //then
+        assertThat(bankServiceSuT.checkBalance()).isEqualTo(currentBalance);
     }
 
     @Test
     public void should_not_allow_to_deposit_over_5000_at_once() {
+        //given
+        Money money = new Money(Currency.PLN, 5001.0);
+        Double currentBalance = getSummaMoney(bankServiceSuT.checkBalance());
+        //when
+        bankServiceSuT.depositMoney(money);
 
+        //then
+        assertThat(getSummaMoney(bankServiceSuT.checkBalance())).isEqualTo(currentBalance);
     }
 
     @Test
     public void should_not_allow_to_withdraw_money_with_different_account_currency() {
+        //given
+        bankServiceSuT.depositMoney(new Money(Currency.PLN, 100.0));
+        Money money = new Money(Currency.EUR, 10.0);
 
+        //when
+        bankServiceSuT.withdrawMoney(money);
+        //then
+        try {
+            //nie wiem co moge tutaj napisac
+        } catch (MoneyException thrown) {
+            assertThat(thrown.getMessage()).isNotEqualTo("");
+        }
     }
 
     @Test
     public void should_not_allow_to_deposit_money_with_different_account_currency() {
+        //given
+        Money money = new Money(Currency.EUR, 14.5);
+        Double currentBalance = getSummaMoney(bankServiceSuT.checkBalance());
+        //when
+        bankServiceSuT.depositMoney(money);
 
+        //then
+        assertThat(getSummaMoney(bankServiceSuT.checkBalance())).isEqualTo(currentBalance);
     }
 }
